54a3df589a0f5101014c813c5219b6a287f86558
Mameden_Q <dynabook.miu@gmail.com>
2019-02-01 00:38:21 +0900

    コード整形

--- aggregater/data2019.js
+++ aggregater/data2019.js
@@ -1,4 +1,4 @@
-var data2019 = 
+var data2019 =
 // {"year":2019,"data":[{"month":1,"data":[{"playerid":"0","spike":{"total":6,"point":1,"miss":2,"etc":3},"serve":{"total":0,"point":0,"miss":0,"etc":0},"receive":{"total":0,"a":0,"b":0,"miss":0,"etc":0},"block":{"total":0,"point":0,"miss":0,"etc":0}},{"playerid":"1","spike":{"total":0,"point":0,"miss":0,"etc":0},"serve":{"total":6,"point":1,"miss":2,"etc":3},"receive":{"total":0,"a":0,"b":0,"miss":0,"etc":0},"block":{"total":0,"point":0,"miss":0,"etc":0}},{"playerid":"2","spike":{"total":0,"point":0,"miss":0,"etc":0},"serve":{"total":0,"point":0,"miss":0,"etc":0},"receive":{"total":0,"a":0,"b":0,"miss":0,"etc":0},"block":{"total":6,"point":1,"miss":2,"etc":3}},{"playerid":"3","spike":{"total":0,"point":0,"miss":0,"etc":0},"serve":{"total":0,"point":0,"miss":0,"etc":0},"receive":{"total":10,"a":1,"b":2,"miss":3,"etc":4},"block":{"total":0,"point":0,"miss":0,"etc":0}}]}]};
 {
   "year": 2019,
--- aggregater/data2020.js
+++ aggregater/data2020.js
@@ -1,89 +1,89 @@
 var data2020 =
-  {
-    year:2020,
-    data:[
-      {
-        month:2,
-        data:[
-          {
-            "playerid": "0",
-            "spike": {
-              "total": 0,
-              "point": 0,
-              "miss": 0,
-              "etc": 0
-            },
-            "serve": {
-              "total": 6,
-              "point": 1,
-              "miss": 2,
-              "etc": 3
-            },
-            "receive": {
-              "total": 0,
-              "a": 0,
-              "b": 0,
-              "miss": 0,
-              "etc": 0
-            },
-            "block": {
-              "total": 0,
-              "point": 0,
-              "miss": 0,
-              "etc": 0
-            }
+{
+  year: 2020,
+  data: [
+    {
+      month: 2,
+      data: [
+        {
+          "playerid": "0",
+          "spike": {
+            "total": 0,
+            "point": 0,
+            "miss": 0,
+            "etc": 0
           },
-                    {
-            // name:"ふじわら",
-            playerid:2,
-            spike:{
-              total:10,
-              point:5,
-              miss:2
-            },
-            serve:{
-              total:10,
-              point:5,
-              miss:4
-            },
-            receive:{
-              total:10,
-              a:2,
-              b:4,
-              miss:1
-            },
-            block:{
-              total:10,
-              point:5,
-              miss:4
-            },
+          "serve": {
+            "total": 6,
+            "point": 1,
+            "miss": 2,
+            "etc": 3
           },
-          {
-            // name:"いけだ",
-            playerid:2,
-            spike:{
-              total:5,
-              point:2,
-              miss:2
-            },
-            serve:{
-              total:10,
-              point:5,
-              miss:4
-            },
-            receive:{
-              total:10,
-              a:2,
-              b:4,
-              miss:1
-            },
-            block:{
-              total:10,
-              point:5,
-              miss:4
-            },
+          "receive": {
+            "total": 0,
+            "a": 0,
+            "b": 0,
+            "miss": 0,
+            "etc": 0
           },
-        ]
-      }
-    ]
-  };
+          "block": {
+            "total": 0,
+            "point": 0,
+            "miss": 0,
+            "etc": 0
+          }
+        },
+        {
+          // name:"ふじわら",
+          playerid: 2,
+          spike: {
+            total: 10,
+            point: 5,
+            miss: 2
+          },
+          serve: {
+            total: 10,
+            point: 5,
+            miss: 4
+          },
+          receive: {
+            total: 10,
+            a: 2,
+            b: 4,
+            miss: 1
+          },
+          block: {
+            total: 10,
+            point: 5,
+            miss: 4
+          },
+        },
+        {
+          // name:"いけだ",
+          playerid: 2,
+          spike: {
+            total: 5,
+            point: 2,
+            miss: 2
+          },
+          serve: {
+            total: 10,
+            point: 5,
+            miss: 4
+          },
+          receive: {
+            total: 10,
+            a: 2,
+            b: 4,
+            miss: 1
+          },
+          block: {
+            total: 10,
+            point: 5,
+            miss: 4
+          },
+        },
+      ]
+    }
+  ]
+};

8f057709d8efa53ac2336719524a5aab44d393f3
Mameden_Q <dynabook.miu@gmail.com>
2019-01-31 22:12:23 +0900

    from mac 分析の一覧はdict単位で表示する

--- aggregater/data2020.js
+++ aggregater/data2020.js
@@ -6,6 +6,34 @@ var data2020 =
         month:2,
         data:[
           {
+            "playerid": "0",
+            "spike": {
+              "total": 0,
+              "point": 0,
+              "miss": 0,
+              "etc": 0
+            },
+            "serve": {
+              "total": 6,
+              "point": 1,
+              "miss": 2,
+              "etc": 3
+            },
+            "receive": {
+              "total": 0,
+              "a": 0,
+              "b": 0,
+              "miss": 0,
+              "etc": 0
+            },
+            "block": {
+              "total": 0,
+              "point": 0,
+              "miss": 0,
+              "etc": 0
+            }
+          },
+                    {
             // name:"ふじわら",
             playerid:2,
             spike:{

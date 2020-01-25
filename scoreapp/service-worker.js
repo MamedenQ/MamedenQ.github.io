502d61f28a0041f6862ede49de3ebd37b23edd68
MamedenQ <dynabook.miu@gmail.com>
2020-01-26 00:49:03 +0900

    配置

--- scoreapp/service-worker.js
+++ scoreapp/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.ea1211169bc9b2fa4437f6727b706133.js"
+  "precache-manifest.c86aced9e2970f95ea136dcf9ba62850.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "scoreapp"});
